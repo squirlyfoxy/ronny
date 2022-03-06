b2dcefd871742b71745fbf98afbd450acfb49901
squirlyfoxy <public.thesquirlyz97@gmail.com>
2022-03-06 15:21:33 +0100

    Get data where id=X

--- database/a-ds_read_data.go
+++ database/a-ds_read_data.go
@@ -4,9 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 )
 
-func GetDataFromATable(database Database, table Table, key int) []string {
+func GetDataFromATable(database Database, table Table, key int) map[string]interface{} {
 	//Check if CAN_GLOBALLY_TAKE
 	contains := false
 
@@ -16,15 +17,18 @@ func GetDataFromATable(database Database, table Table, key int) []string {
 		}
 	}
 	if !contains {
-		fmt.Println("You don't have access to this table")
-		return []string{}
+		return map[string]interface{}{
+			"error": "You can't take this data",
+		}
 	}
 
 	//Read the JSON (./db/data/[tableName].dat.json)
 	file, err := os.Open("./db/data/" + table.Name + ".dat.json")
 	if err != nil {
 		fmt.Println(err)
-		return []string{}
+		return map[string]interface{}{
+			"error": "Error while opening the file",
+		}
 	}
 	defer file.Close()
 
@@ -33,14 +37,47 @@ func GetDataFromATable(database Database, table Table, key int) []string {
 	err = json.NewDecoder(file).Decode(&tableData)
 	if err != nil {
 		fmt.Println(err)
-		return []string{}
+		return map[string]interface{}{}
 	}
 
-	//Check the lenght of the data
-	if len(tableData.Data) < key || len(tableData.Data) == 0 {
-		return []string{}
+	//Check if the key is valid
+	if key < 0 || key >= len(tableData.Data) {
+		return map[string]interface{}{
+			"error": "The key is not valid",
+		}
 	}
 
-	//Return the data
-	return tableData.Data[key]
+	data := tableData.Data[key]
+
+	//Build the return ads JSON (get columns name from table)
+	var ret map[string]interface{}
+	//Initialize the map
+	ret = make(map[string]interface{})
+
+	var i int = 0
+
+	for _, column := range table.Columns {
+		current := data[i]
+
+		//Check if the columnt is int or float
+		if column.Type == INT || column.Type == FLOAT || (column.Type == KEY && column.Rule == AUTOINCREMENT) {
+			//Is a JSON, [tName]:[value]
+
+			//Convert current in a int
+			var currentInt int
+			currentInt, err = strconv.Atoi(current)
+			if err != nil {
+				fmt.Println(err)
+				return map[string]interface{}{}
+			}
+
+			ret[column.Name] = currentInt
+		} else {
+			ret[column.Name] = current
+		}
+
+		i++
+	}
+
+	return ret
 }
