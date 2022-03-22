9b92931cd8f613c70b62931f7fea82c2e229ad34
squirlyfoxy <public.thesquirlyz97@gmail.com>
2022-03-22 16:13:59 +0100

    from configuration.ini to ocnfig.ini

--- database/config_ini.go
+++ database/config_ini.go
@@ -16,7 +16,7 @@ type Config struct {
 func ReadConfiguration() Config {
 	var c Config
 
-	cfg, err := ini.Load("./db/configuration.ini")
+	cfg, err := ini.Load("./db/config.ini")
 	if err != nil {
 		fmt.Printf("Fail to read file: %v", err)
 		os.Exit(1)
@@ -25,6 +25,5 @@ func ReadConfiguration() Config {
 	c.Port = cfg.Section("ads").Key("port").MustInt()
 	c.Host = cfg.Section("ads").Key("host").String()
 	c.DoDefaultRoute = cfg.Section("ads").Key("do_default_route").MustInt()
-
 	return c
 }
